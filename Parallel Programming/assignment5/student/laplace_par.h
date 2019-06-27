5580478b41b1ed8658b78a58c41cffec458c0845
Kaan Atukalp <katukalp@gmail.com>
2019-06-27 10:59:54 +0200

    Add files via upload

---
+++ Parallel Programming/assignment5/student/laplace_par.h
@@ -0,0 +1,43 @@
+#ifndef _LAPLACE_PAR_
+#define _LAPLACE_PAR_
+
+#include<omp.h>
+
+template<int SIZE>
+inline void initialize(double a[SIZE + 2][SIZE + 2], double b[SIZE + 2][SIZE + 2]) {
+	//TODO implement your solution in here
+	#pragma omp parallel for
+	for (int i = 0; i < SIZE + 2; i++)
+        for (int j = 0; j < SIZE + 2; j++)
+        {
+            a[i][j] = 0.0;
+            b[i][j] = 0.0;
+        }
+    #pragma omp barrier
+}
+
+template<int SIZE>
+inline void time_step(double a[SIZE + 2][SIZE + 2], double b[SIZE + 2][SIZE + 2], int n) {
+	//TODO implement your solution in here
+	omp_set_nested(1);
+	
+    #pragma omp parallel shared(n)
+    {
+        if (n % 2 == 0)
+        {
+            #pragma omp for
+            for (int i = 1; i < SIZE + 1; i++)
+                for (int j = 1; j < SIZE + 1; j++)
+                    b[i][j] = (a[i + 1][j] + a[i - 1][j] + a[i][j - 1] + a[i][j + 1]) / 4.0;
+        }
+        else
+        {
+            #pragma omp for 
+            for (int i = 1; i < SIZE + 1; i++)
+                for (int j = 1; j < SIZE + 1; j++)
+                    a[i][j] = (b[i + 1][j] + b[i - 1][j] + b[i][j - 1] + b[i][j + 1]) / 4.0;
+        }
+    }
+}
+
+#endif // !_LAPLACE_PAR_
